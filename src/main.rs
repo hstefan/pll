1f9a51388ef2a73fb04109f1fe673e33db877fde
Stefan Puhlmann <hugopuhlmann@gmail.com>
2022-12-10 02:45:07 +0100

    support splitting by several characters

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use std::io::{BufRead, Read};
+
 use std::str;
 use std::time::Duration;
 use std::{process, thread};
@@ -143,16 +144,74 @@ impl ProcPool {
     }
 }
 
+trait ManySplit<B> {
+    fn split_any(self, delims: &[u8]) -> SplitMany<B>;
+}
+
+struct SplitMany<B> {
+    buf: B,
+    delims: Vec<u8>,
+    next: Vec<u8>,
+}
+
+impl<B: BufRead> Iterator for SplitMany<B> {
+    type Item = std::io::Result<Vec<u8>>;
+
+    fn next(&mut self) -> Option<std::io::Result<Vec<u8>>> {
+        loop {
+            if let Some(pos) = self.next.iter().position(|x| self.delims.contains(x)) {
+                // TODO: don't re-scan characters that we already checked
+                return Some(Ok(self.next.drain(0..pos + 1).collect()));
+            }
+            let amt = match self.buf.fill_buf() {
+                Ok(bytes) => {
+                    self.next.extend_from_slice(bytes);
+                    bytes.len()
+                }
+                Err(e) => return Some(Err(e)),
+            };
+            self.buf.consume(amt);
+            if amt == 0 {
+                return if self.next.is_empty() {
+                    None
+                } else {
+                    Some(Ok(self.next.drain(0..).collect()))
+                };
+            }
+        }
+    }
+}
+
+impl<B: BufRead> ManySplit<B> for B {
+    fn split_any(self, delims: &[u8]) -> SplitMany<B> {
+        SplitMany {
+            buf: self,
+            delims: delims.to_vec(),
+            next: vec![],
+        }
+    }
+}
+
+fn clean_arg<'a>(delims: &[u8], arg: &'a [u8]) -> Option<&'a str> {
+    if let Some(start) = arg.iter().position(|x| !delims.contains(x)) {
+        Some(str::from_utf8(&arg[start..]).expect("argument decoding failed"))
+    } else {
+        None
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
-    let delim = args.delim.map_or_else(|| b'\n', |v| v.as_bytes()[0]);
+    let delim = args
+        .delim
+        .map_or_else(|| vec![b'\n', b'\t', b' '], |v| v.as_bytes().to_vec());
     let program = args.program.get(0).map(AsRef::as_ref).unwrap_or("echo");
     let initial_args = args.program.iter().skip(1).map(|v| v.to_owned()).collect();
 
     let stdin = std::io::stdin();
     let reader = stdin.lock();
-    let split_iter = reader.split(delim);
+    let split_iter = reader.split_any(&delim);
 
     let mut pool = ProcPool::new(
         program.into(),
@@ -163,8 +222,9 @@ fn main() {
     );
     for result in split_iter {
         let buf = result.expect("failed to read argument buf");
-        let arg = str::from_utf8(&buf).expect("argument decoding failed");
-        pool.push_arg(arg);
+        if let Some(arg) = clean_arg(&delim, &buf) {
+            pool.push_arg(arg);
+        }
     }
     pool.wait_all()
 }
